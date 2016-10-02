095710aa17bd17911735459b145e805e100e26b1
marlboroyw@naver.com <marlboroyw@naver.com>
2016-10-02 15:44:23 +0900

    YW_board_20161002

--- router/main.js
+++ router/main.js
@@ -1,6 +1,11 @@
 module.exports = function(app, fs, connection,util,db_query,formidable)
 {
-    app.get('/',function(req,res){
+  app.get('/',function(req,res){
       res.render('views/index.html');
+    });
+
+  app.get('/board', function(req, res)
+    {
+      res.render('views/board/board.html');
     });
 }
