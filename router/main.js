928dfde1c0b602dd36b187aeb2e7eff7a296aa21
marlboroyw@naver.com <marlboroyw@naver.com>
2016-10-03 22:25:47 +0900

    이영우 듸비 성공

--- router/main.js
+++ router/main.js
@@ -1,4 +1,4 @@
-module.exports = function(app, fs, connection,util,db_query,formidable)
+module.exports = function(app, fs, connection, util, db_query, formidable)
 {
   app.get('/',function(req,res){
       res.render('views/index.html');
@@ -9,16 +9,37 @@ module.exports = function(app, fs, connection,util,db_query,formidable)
     res.render('views/board/board.html');
   });
 
-  app.get('/loadpage' function(req, res)
+  app.get('/loadpage', function(req, res)
   {
     var pagename = req.query.pagename;
 
     console.log("page name : " + pagename);
 
-    var url = "/" + pagename + ".html";
+    var url = "views/board/" + pagename + ".html";
 
     console.log("url : " + url);
+    
+    
+    // DB 가져오기
+    var query = util.format(db_query.query.select.maruSelect, "1");
 
-    res.render(url);
+    connection.query(query, function(err, rows)
+    {
+      var result  = {};
+
+
+      console.log(result);
+
+      if(rows.length > 0)
+      {
+        console.log(rows);
+      }
+      else
+      {
+        console.log("failed : " + rows);
+      }
+
+      res.render(url, rows[0]);
+    });
   });
 }
